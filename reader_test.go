a3adcaa9d10593677e5b6fb9149ef03090940b32
thisisaaronland <aaron@localhost>
2021-10-20 17:18:42 -0700

    complete tests

--- reader_test.go
+++ reader_test.go
@@ -1,9 +1,72 @@
 package csvdict
 
 import (
+	"bufio"
+	"io"
+	"os"
 	"testing"
 )
 
 func TestReader(t *testing.T) {
-	t.Skip()
+
+	path := "fixtures/test.csv"
+
+	fh, err := os.Open(path)
+
+	if err != nil {
+		t.Fatalf("Failed to open %s, %v", path, err)
+	}
+
+	defer fh.Close()
+
+	scanner := bufio.NewScanner(fh)
+	count_lines := 0
+
+	for scanner.Scan() {
+		count_lines += 1
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		t.Fatalf("Scanner reported an error, %v", err)
+	}
+
+	_, err = fh.Seek(0, 0)
+
+	if err != nil {
+		t.Fatalf("Failed to seek file to 0, %v", err)
+	}
+
+	csv_r, err := NewReader(fh)
+
+	if err != nil {
+		t.Fatalf("Failed to create reader, %v", err)
+	}
+
+	count_rows := 0
+
+	for {
+		row, err := csv_r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			t.Fatalf("Failed to read row, %v", err)
+		}
+
+		_, ok := row["label"]
+
+		if !ok {
+			t.Fatalf("Row is missing 'label' column")
+		}
+
+		count_rows += 1
+	}
+
+	if count_rows != count_lines-1 {
+		t.Fatalf("Expected %d rows, but got %d", count_lines-1, count_rows)
+	}
 }
--- writer_test.go
+++ writer_test.go
@@ -1,9 +1,96 @@
 package csvdict
 
 import (
+	"bufio"
+	"bytes"
+	_ "fmt"
+	"io"
+	"os"
+	"sort"
 	"testing"
 )
 
 func TestWriter(t *testing.T) {
-	t.Skip()
+
+	path := "fixtures/test.csv"
+
+	fh, err := os.Open(path)
+
+	if err != nil {
+		t.Fatalf("Failed to open %s, %v", path, err)
+	}
+
+	defer fh.Close()
+
+	body, err := io.ReadAll(fh)
+
+	if err != nil {
+		t.Fatalf("Failed to read body for %s, %v", path, err)
+	}
+
+	_, err = fh.Seek(0, 0)
+
+	if err != nil {
+		t.Fatalf("Failed to seek file to 0, %v", err)
+	}
+
+	csv_r, err := NewReader(fh)
+
+	if err != nil {
+		t.Fatalf("Failed to create reader, %v", err)
+	}
+
+	rows := make([]map[string]string, 0)
+
+	for {
+		row, err := csv_r.Read()
+
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			t.Fatalf("Failed to read row, %v", err)
+		}
+
+		rows = append(rows, row)
+	}
+
+	var buf bytes.Buffer
+	wr := bufio.NewWriter(&buf)
+
+	fieldnames := make([]string, 0)
+
+	for k, _ := range rows[0] {
+		fieldnames = append(fieldnames, k)
+	}
+
+	sort.Strings(fieldnames)
+
+	csv_wr, err := NewWriter(wr, fieldnames)
+
+	if err != nil {
+		t.Fatalf("Failed to create new writer, %v", err)
+	}
+
+	err = csv_wr.WriteHeader()
+
+	if err != nil {
+		t.Fatalf("Failed to write CSV header, %v", err)
+	}
+
+	for i, row := range rows {
+
+		err := csv_wr.WriteRow(row)
+
+		if err != nil {
+			t.Fatalf("Failed to write row (%d), %v", i, err)
+		}
+
+		csv_wr.Flush()
+	}
+
+	if !bytes.Equal(body, buf.Bytes()) {
+		t.Fatalf("Unexpected output")
+	}
 }
